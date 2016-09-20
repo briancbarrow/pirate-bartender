e152d2e0648753c77781d862fc12b26b95c5d5f0
Brian Barrow <briancbarrow@gmail.com>
2016-09-19 21:51:33 -0600

    added question rendering

--- app.js
+++ app.js
@@ -6,16 +6,33 @@ $(document).ready(function() {
     this.sweetness = sweetness;
     this.fruity = fruity;
   };
+  Pantry.prototype.addToPantry = function(ingredient, name) {
+      if(this[name]) {
+        this[name].push(ingredient);
+      } else {
+        this[name] = [];
+        this[name].push(ingredient);
+      }
+    };
+
   var Ingredients = function(ingredients) {
     this.ingredients = ingredients;
   }
-  var Question = function(ingredients, name) {
-    this.question = "Which 'o these" + name + "in'redients do ye want in yer drink?";
-    this.ingredients = ingredients;
+  var Questions = function(pantry) {
+    this.counter = 0;
+    this.questions = [];
+    for(variable in pantry){
+      if(pantry.hasOwnProperty(variable)) {
+        var question = "Do ye want yer drink to be " + variable + "?";
+        this.questions.push({question, answer: '', name: variable});
+      }
+    }
   }
-  var Drink = function(ingredientChoices){
-    this.ingredients = ingredientChoices;
-  }
+
+
+  // var Drink = function(ingredients) {
+
+  // }
 
   var strongIngredients = new Ingredients(['Glug of rum', 'slug of whisky', 'splash of gin']);
   var saltyIngredients = new Ingredients(['Olive on a stick', 'salt-dusted rim', 'rasher of bacon']);
@@ -23,7 +40,28 @@ $(document).ready(function() {
   var sweetIngredients = new Ingredients(['Sugar cube', 'spoonful of honey', 'splash of cola']);
   var fruityIngredients = new Ingredients(['Slice of orange', 'dash of cassis', 'cherry on top']);
 
-  var pantry = new Pantry(strongIngredients, saltyIngredients, bitterIngredients, sweetIngredients, fruityIngredients);
+  var myPantry = new Pantry(strongIngredients, saltyIngredients, bitterIngredients, sweetIngredients, fruityIngredients);
+
+  var myQuestions = new Questions(myPantry);
+
+  var myDrink = {
+    renderQuestion: function() {
+      $('#question').text(myQuestions.questions[myQuestions.counter].question);
+    },
+    getRandomNumber: function(length) {
+      Math.floor(Math.random() * length);
+    }
+  };
+
+  $('#yes').click(function(){
+    var name = myQuestions.questions[myQuestions.counter].name;    
+    var length = myPantry[name].ingredients.length;
+    var randNum = myDrink.getRandomNumber(length);
+    myQuestions.questions[myQuestions.counter].answer = true;
+    console.log(myQuestions.questions);
+  });
   
-  
+  myDrink.renderQuestion();
+  console.log(myQuestions.questions[myQuestions.counter].answer)
+
 });
