cf578d8ff02bfb08cc869fb20b3f2e128659e3ef
Brian Barrow <briancbarrow@gmail.com>
2016-09-22 22:05:51 -0600

    added customer recognition, need to finish its functionality

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  var customers = {};
   var Pantry = function() {
     this.ingredients = {};
     this.addToPantry = function(ingredient) {
@@ -6,20 +7,24 @@ $(document).ready(function() {
         if(Array.isArray(ingredient.name)) {
           for(i = 0; i < ingredient.name.length; i++){
             this.ingredients[ingredient.type].push(ingredient.name[i]);
-          }
+          };
         } else {
           this.ingredients[ingredient.type].push(ingredient.name);
-        }
+        };
       } else {
         this.ingredients[ingredient.type] = [ingredient.name];
-      }
-    }
+      };
+    };
+  };
+
+  var Customer = function(name) {
+    this.name = name;
   };
 
   var Ingredient = function(type, name) {
     this.type = type;
     this.name = name;
-  }
+  };
 
   var Questions = function(pantry, typeArr, phrase) {
     this.questions = {};
@@ -32,9 +37,9 @@ $(document).ready(function() {
     var randNumAdj = Math.floor(Math.random() * length);
     var randNumNoun = Math.floor(Math.random() * length);
     this.displayItem = function() {
-      var display = '"' + this.adjectives[randNumAdj] + ' ' + this.nouns[randNumNoun] + '"';
-      $('#question').text('Enjoy yer ' + display);
-    }
+      this.display = this.adjectives[randNumAdj] + ' ' + this.nouns[randNumNoun];
+      $('#question').text('Enjoy yer "' + this.display + '"');
+    };
   };
 
   var Preferences = function() {
@@ -42,13 +47,13 @@ $(document).ready(function() {
     this.addPreference = function(preference) {
       this.preferences.push(preference);
     };
-  }
+  };
 
   var Worker = function(pantry, typeArr, phrase) {
     this.ingredients = {};
     for(i = 0; i < typeArr.length; i++) {
       this.ingredients[typeArr[i]] = pantry.ingredients[typeArr[i]]
-    }
+    };
     this.questionCounter = 0;
     this.questionList = new Questions(pantry, typeArr, phrase)
     this.questionsLength = Object.keys(this.questionList.questions).length
@@ -59,17 +64,16 @@ $(document).ready(function() {
         $('#question').text(this.questionList.questions[typeArr[this.questionCounter]]);
         $('#burger, #drink').hide();
         $('#yes, #no').show();
-      }
+      };
     };
     this.makeItem = function(preferencesLength) {
-      console.log('making')
-      var item = new Item(preferencesLength);      
-        item.adjectives = this.adjectives;
-        item.nouns = this.nouns;
-        console.log(item);
-        item.displayItem();
-      }
-    }
+      this.item = new Item(preferencesLength);      
+      this.item.adjectives = this.adjectives;
+      this.item.nouns = this.nouns;
+      this.item.displayItem();
+      console.log(this.item.display)
+    };
+  };
 
   var strongIngredients = new Ingredient('strong', ['Glug of rum', 'slug of whisky', 'splash of gin']);
   var saltyIngredients = new Ingredient('salty', ['Olive on a stick', 'salt-dusted rim', 'rasher of bacon']);
@@ -98,41 +102,90 @@ $(document).ready(function() {
   var chef = new Worker(myPantry, ['meat', 'lettuce', 'tomato', 'cheese', 'sauce'], 'Do you like your burger with ')
   var myPreferences = new Preferences();
   var worker;
+  var customerName;
+  
+
+  $('#customer-info').submit(function(e) {
+    e.preventDefault();    
+    var nameCheck = false;
+    customerName = $('#customer-name').val();
+    $('#customer-info').hide();
+    if(Object.keys(customers).length > 0) {
+      for(property in customers) {
+        console.log(property)
+        if(property === customerName) {
+          nameCheck = true;
+          // console.log(customers);
+          break;
+        } else {
+          nameCheck = false;
+        }
+      }
+    }
+    
+    
+    if(customerName !== null) {
+      if(nameCheck === false) {
+        $('#burger, #drink, #question').show();
+        customer = new Customer(customerName);
+        customers[customerName] = customer;
+        console.log(customers);
+        console.log(customers[customerName].name);
+        console.log(Object.keys(customers).length);
+      } else {
+        $('#question').text('Hi ' + customers[customerName].name + ', do you want your usual?')
+        $('#question').show();
+      }
+    }
+  });
 
   $('#burger').click(function() {
     worker = chef;
-    chef.adjectives = ['Mighty', 'Ruthless', 'Marooned', 'Pillaging', 'Big'];
+    worker.product = 'burger';
+    chef.adjectives = ['Mighty', 'Ruthless', "Dead Man's", 'Pillaging', 'Big'];
     chef.nouns = ['Mouth', 'Vesssel', 'Treasure', 'Scoundrel', 'Kahuna'];
     worker.renderQuestion();
-  })
+  });
   $('#drink').click(function() {
     worker = bartender;
+    worker.product = 'drink';
     bartender.adjectives = ['Limey', 'Ruthless', 'Marooned', 'Pillaging', 'Swashbuckling'];
     bartender.nouns = ['Walk the Plank', 'Ruffian', 'Treasure Island', 'Galleon', 'Port'];
     worker.renderQuestion();
-  })
+  });
 
   $('#yes').click(function() {
-    var name = worker.name;  
+    var ingName = worker.name;  
     var ingLength = Object.keys(worker.ingredients).length;    
     var randNum = Math.floor(Math.random() * ingLength);
-    myPreferences.addPreference(worker.ingredients[name][0][randNum]);
+    myPreferences.addPreference(worker.ingredients[ingName][0][randNum]);
     worker.questionCounter++;
     if(worker.questionCounter >= worker.questionsLength) {
-      $('button').hide();
-      worker.makeItem(myPreferences.preferences.length)
+      $('#yes, #no').hide();
+      worker.makeItem(myPreferences.preferences.length);
+      customers[customerName][worker.product] = worker.item.display;
+      console.log(customers);
+      $('#new').show();
     } else {
       worker.renderQuestion();
-    }    
+    };  
   });
-  $('#no').click(function(){
+  $('#no').click(function() {
     worker.questionCounter++;
     if(worker.questionCounter >= worker.questionsLength) {
-      $('button').hide();
+      $('#yes, #no').hide();
       worker.makeItem(myPreferences.preferences.length)
+      customers[customerName][worker.product] = worker.item.display;
+      $('#new').show();
     } else {
       worker.renderQuestion();
-    }
+    };
   });
-  
+  $('#new').click(function() {
+    $('#question').hide();
+    $('#new').hide();
+    $('#customer-info').show();
+  })
+  // console.log(customers.length)
+  // console.log(customers)
 });
